cf6e9305d25c3a36db051757f96ffd9f34acf3ac
premrobosoft <premnath@robosoftin.com>
2020-11-04 09:35:33 +0530

    User icons aded from local directry

--- src/shared/contactList.ts
+++ src/shared/contactList.ts
@@ -2,52 +2,52 @@ import { IuserInfo } from './MyInterface';
 
 export const contactList: IuserInfo[] = [
     {
-        name: "Premnath",
+        name: "Premnath kulal",
         phoneNo: 8970296514,
         email: "pkctech1999@gmail.com",
         adress: "Mangaluru",
-        userImage: "https://static.thenounproject.com/png/17241-200.png"
+        userImage: "../assets/Images/userIcon.png"
     },
     {
         name: "Rakshith",
         phoneNo: 8970296514,
         email: "pkctech1999@gmail.com",
         adress: "Bantwala",
-        userImage: "https://static.thenounproject.com/png/17241-200.png"
+        userImage: "../assets/Images/userIcon.png"
     },
     {
         name: "Rama",
         phoneNo: 8970296514,
         email: "pkctech1999@gmail.com",
         adress: "Mani",
-        userImage: "https://static.thenounproject.com/png/17241-200.png"
+        userImage: "../assets/Images/userIcon.png"
     },
     {
         name: "Venkat",
         phoneNo: 8970296514,
         email: "pkctech1999@gmail.com",
         adress: "Kalladka",
-        userImage: "https://static.thenounproject.com/png/17241-200.png"
+        userImage: "../assets/Images/userIcon.png"
     },
     {
         name: "Venkat",
         phoneNo: 8970296514,
         email: "pkctech1999@gmail.com",
         adress: "Kalladka",
-        userImage: "https://static.thenounproject.com/png/17241-200.png"
+        userImage: "../assets/Images/userIcon.png"
     },
     {
         name: "Venkat",
         phoneNo: 8970296514,
         email: "pkctech1999@gmail.com",
         adress: "Kalladka",
-        userImage: "https://static.thenounproject.com/png/17241-200.png"
+        userImage: "../assets/Images/userIcon.png"
     },
     {
         name: "Venkat",
         phoneNo: 8970296514,
         email: "pkctech1999@gmail.com",
         adress: "Kalladka",
-        userImage: "https://static.thenounproject.com/png/17241-200.png"
+        userImage: "../assets/Images/userIcon.png"
     }
 ]
